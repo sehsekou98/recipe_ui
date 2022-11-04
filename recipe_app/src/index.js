7428b193072116366e8237350c23f1d590f54726
Sekou S. Seh <sehsekou98@gmail.comh>
2022-11-04 13:04:44 +0100

    install react-querry & axios

--- recipe_app/src/index.js
+++ recipe_app/src/index.js
@@ -8,13 +8,22 @@ import "@fontsource/inter"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import App from './App';
 
 
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <QueryClientProvider client={queryClient}>
+      <App/>
+    </QueryClientProvider>
   </React.StrictMode>
 );
